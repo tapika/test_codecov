75a91c621018348374f650534f2b36c43472cbde
Tarmo Pikaro <tapika@yahoo.com>
2021-12-21 19:50:04 +0200

    add missing tool installation

--- cakebuild/Program.cs
+++ cakebuild/Program.cs
@@ -14,6 +14,8 @@ namespace cakebuild
             //host.InstallTool(new Uri("dotnet:?package=coverlet.console&version=3.1.0"));
             // Needed for testVsTest
             host.InstallTool(new Uri("nuget:?package=Microsoft.CodeCoverage&version=17.0.0"));
+            // Needed for all tools
+            host.InstallTool(new Uri("nuget:?package=ReportGenerator&version=5.0.0"));
             List<String> args = _args.ToList();
 
             if (args.Contains("--show"))
